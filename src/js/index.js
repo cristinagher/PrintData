9cc17d4e6cad01381a80290e015a63d3acf5a7b8
Cristina Gámez <163881246+cristinagher@users.noreply.github.com>
2024-06-19 18:59:56 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
+
+//- Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
+
+const numbers = [1, 2, 3, 4];
+const primerEjercicio = numbers.map((number) => {
+  return number * numbers.indexOf();
+});
+console.log(primerEjercicio);
